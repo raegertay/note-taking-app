0d72312dbdf58c3851f7fd879c785310444aab30
raegertay <raegertay89@gmail.com>
2017-11-21 00:27:22 +0800

    Added delete function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super(props);
     this.state = {
       term: '',
-      items: [],
+      items: new Map(),
+      item_id: 1,
     }
     // this.onChange = this.onChange.bind(this);
   }
@@ -18,13 +19,25 @@ class App extends Component {
 
   onSubmit = (event) => {
     event.preventDefault();
+    const new_items = new Map(this.state.items);
+    new_items.set(this.state.item_id, this.state.term);
     this.setState({
       term: '',
-      items: [...this.state.items, this.state.term],
+      items: new_items,
+      item_id: (this.state.item_id + 1),
     });
   }
 
+  onClick = (event) => {
+    const item_id = parseInt(event.target.id);
+    const new_items = new Map(this.state.items);
+    new_items.delete(item_id);
+    this.setState({items: new_items});
+  }
+
   render() {
+    let li_arr = [];
+    this.state.items.forEach((term, item_id) => li_arr.push([item_id, term]));
     return (
       <div>
         <form className='App' onSubmit={this.onSubmit}>
@@ -32,6 +45,9 @@ class App extends Component {
           <button>Submit</button>
         </form>
         <List items={this.state.items} />
+        {
+          li_arr.map((pair, idx) => <button key={pair[0]} id={pair[0]} onClick={this.onClick}>Delete #{idx+1}</button>)
+        }
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
-const List = (props) => (
-  <ol>
-    {
-      props.items.map(item => <li>{item}</li>)
-    }
-  </ol>
-)
+// const List = (props) => (
+//   <ol>
+//   {
+//     props.items.forEach((item, item_id) => <li key={item_id}>{item}</li>)
+//   }
+//   </ol>
+// )
+
+class List extends Component {
+  render() {
+    let li_arr = [];
+    this.props.items.forEach((term, item_id) => li_arr.push([item_id, term]));
+    return (
+      <ol>
+      {
+        li_arr.map((pair) => <li key={pair[0]}>{pair[1]}</li>)
+      }
+      </ol>
+    )
+  }
+}
 
 export default List;
